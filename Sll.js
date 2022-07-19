ccbab5f744b36328f990913ba88c2366584a962c
Fenguin Phoenix <73833580+Phoenixz-py@users.noreply.github.com>
2022-07-19 20:03:22 +0530

    Update Sll.js

--- Sll.js
+++ Sll.js
@@ -1,52 +1,53 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync({sigint: true})
-console.log('Welcome to my program to create a linked list')
 class Node {
-    constructor(data, next = null) {
-      this.data = data;
-      this.next = next;
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class List {
+  constructor(head = null) {
+    this.head = head
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  append(value) {
+    const node = new Node(value);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      let last_node = this.head;
+
+      while (last_node.next !== null) {
+        last_node = last_node.next;
+      }
+
+      last_node.next = node;
+    }
+
+    return this;
+  }
+
+  *[Symbol.iterator]() {
+    let current = this.head
+
+    while (current !== null) {
+      yield current.data
+
+      current = current.next;
     }
   }
-  
-  class List {
-    constructor(head = null) {
-      this.head = head
-    }
-  
-    getFirst() {
-      return this.head;
-    }
-  
-    append(value) {
-      const node = new Node(value);
-  
-      if (this.head === null) {
-        this.head = node;
-      } else {
-        let last_node = this.head;
-  
-        while (last_node.next !== null) {
-          last_node = last_node.next;
-        }
-  
-        last_node.next = node;
-      }
-  
-      return this;
-    }
-  
-    *[Symbol.iterator]() {
-      let current = this.head
-  
-      while (current !== null) {
-        yield current.data
-  
-        current = current.next;
-      }
-    }
-  }
-  const askerString = "Enter value you want to make it present in the linked list: ";
+}
+
+let askerString = "Enter value you want to make it present in the linked list: ";
 
   let list = new List().append(+prompt(askerString)).append(+prompt(askerString)).append(+prompt(askerString)).append(+prompt(askerString));
-  
-  console.log([...list])
+// let list = new List().append(3).append(4).append(6).append(8);
+
+console.log([...list])
